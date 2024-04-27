8bf0f83cb3943cd504352464ec8563f697a593d9
MassHesteria <brad.hester@gmail.com>
2024-04-27 10:28:27 -0500

    Better gif encoding

--- app/image/[slug]/route.tsx
+++ app/image/[slug]/route.tsx
@@ -1,83 +1,58 @@
 import { NextRequest, NextResponse } from 'next/server';
-import sharp from 'sharp'
-const GIFEncoder = require('gifencoder')
-
-const GIF_from_Frames = (
-   width: number,
-   height: number,
-   frames: any
-) => {
-  const encoder = new GIFEncoder(width, height);
-
-  encoder.start();
-  encoder.setRepeat(0); // 0 for infinite loop
-  encoder.setDelay(1000); // Frame delay in ms
-
-  // Add each buffer as a frame
-  frames.forEach((f: any) => {
-    encoder.addFrame(f);
-  });
-
-  encoder.finish();
-
-  return encoder.out.getData();
-};
-
-type Pixel = {
-  x: number;
-  y: number;
-}
-
-const getFrame = async (width: number, height: number, pixels: Pixel[]) => {
-   return sharp({
-      create: {
-         width,
-         height,
-         channels: 4,
-         background: { r: 230, g: 230, b: 230, alpha: 1.0 },
-      },
-   })
-      .composite(
-         pixels.map((p) => {
-            return {
-               input: {
-                  create: {
-                     width: 1,
-                     height: 1,
-                     channels: 4,
-                     background: { r: 0, g: 0, b: 0, alpha: 1 },
-                  },
-               },
-               left: p.x,
-               top: p.y,
-            };
-         })
-      )
-      .toBuffer();
-};
+const { GIFEncoder } = require('gifenc');
 
 export async function GET(
    req: NextRequest,
-   { params }: { params: { slug: string } }
+   { params }: { params: { slug: string}}
 ) {
-  const width = 24
-  const height = 24
+  const size = 356
+  const num = 20
+
+  const rows = Math.floor(size/num)
+  const cols = rows
 
   const { slug } = params;
   console.log(slug);
 
-  const num = parseInt(slug, 16)
-  const a = num % width
-  const b = Math.floor(num / width)
+  const cnt = parseInt(slug, 16)
+  const a = cnt % cols
+  const b = Math.floor(cnt / rows)
 
-  const arr = await Promise.all([
-    getFrame(width, height, [{ x: a, y: b}, { x: a+1, y: b}]),
-    getFrame(width, height, [{ x: a, y: b+1}, { x: a+1, y: b+1}]),
-    getFrame(width, height, [{ x: a, y: b+2}, { x: a+1, y: b+2}]),
-  ])
+  console.log('row:',b,'col:',a)
 
-  const imageBuffer = GIF_from_Frames(width, height, arr);
-  const response = new NextResponse(imageBuffer);
+  const palette = [
+    [240, 240, 240],
+    [200, 200, 200],
+    [40, 220, 220],
+  ]
+
+  const grid = new Uint8Array(size*size)
+
+  for (let i = num; i < size; i += (num+1)) {
+    for (let j = 0; j < size; j++) {
+      grid[(j*size) + i] = 1
+      grid[(i*size) + j] = 1
+    }
+  }
+
+  const setCell = (row: number, col: number, color: number) => {
+    const idx = (row * size * (num + 1)) + (col * (num + 1))
+
+    for (let i = 0; i < num; i++) {
+      for (let j = 0; j < num; j++) {
+        grid[idx + i + (size * j)] = color
+      }
+    }
+  }
+
+  const gif = GIFEncoder()
+  for (let i = 0; i < 15; i++) {
+    setCell(b + i, a + i, 2)
+    gif.writeFrame(grid, size, size, { palette, delay: 1000 })
+  }
+  gif.finish()
+
+  const response = new NextResponse(gif.bytesView());
   response.headers.set("content-type", "image/gif");
   response.headers.set("Cache-Control", "no-store, max-age=0");
   return response;
