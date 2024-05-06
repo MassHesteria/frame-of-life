b2287a473cb9a53424b6c095dcd03955500da52b
MassHesteria <brad.hester@gmail.com>
2024-05-06 07:14:16 -0500

    Added basic web page

--- app/page.tsx
+++ app/page.tsx
@@ -31,8 +31,15 @@ export async function generateMetadata({ searchParams }: Props) {
 
 export default async function Page() {
   return (
-    <div className="pl-2 pt-2">
-      <div>Mic check</div>
+    <div className="pl-4 pt-4">
+      <center>
+      <h1 className="pb-2 text-4xl">Frame of Life <span className="text-2xl">by MassHesteria</span></h1>
+      <div>
+        <a className="text-red-600 text-2xl no-underline hover:underline pr-8" href="https://github.com/masshesteria/frame-of-life">Source code</a>
+        <a className="text-purple-600 text-2xl no-underline hover:underline" href="https://warpcast.com/masshesteria/0x3c45cf6b">Original cast</a>
+      </div>
+      <img className="mt-4 border border-black" alt="Dart" src="/image/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAoAACIAADgAAAAAAxgAEUQAYowBBQQBatAAAAAAAAAA=="></img>
+      </center>
     </div>
   )
 }
